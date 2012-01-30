1bfdb6eaa63af8237981fe2f4f251789081f2794
Matheus Boy <matheusboy.mbs@gmail.com>
2012-01-30 12:42:53 -0200

    almost fully replacing tar(1) with zipfile

--- create_zip_folder.py
+++ create_zip_folder.py
@@ -1,31 +1,33 @@
-import zipfile
-import os
+import zipfile, os, sys
 
 # this shit adds a fucking folder to a zip file
 class ZipUtilities:
+    # this stuff now takes a list as arg
+    def toZip(self, files_to_zip, filename):
+        zip_file = zipfile.ZipFile(filename, "w")
+        for file_to_zip in files_to_zip:
+            if os.path.isfile(file_to_zip):
+                zip_file.write(file_to_zip)
+            else:
+                self.addFolderToZip(zip_file, file_to_zip)
+        zip_file.close()
 
-	def toZip(self, file, filename):
-		zip_file = zipfile.ZipFile(filename, "w")
-		if os.path.isfile(file):
-			zip_file.write(file)
-		else:
-			self.addFolderToZip(zip_file, file)
-		zip_file.close()
-
-	def addFolderToZip(self, zip_file, folder): 
-		for file in os.listdir(folder):
-			full_path = os.path.join(folder, file)
-			if os.path.isfile(full_path):
-				#print "File added: " + str(full_path)
-				zip_file.write(full_path)
-			elif os.path.isdir(full_path):
-				#print "Entering folder: " + str(full_path)
-				self.addFolderToZip(zip_file, full_path)
+    def addFolderToZip(self, zip_file, folder): 
+        for file_ in os.listdir(folder):
+            full_path = os.path.join(folder, file_)
+            if os.path.isfile(full_path):
+                #print "File added: " + str(full_path)
+                zip_file.write(full_path)
+            elif os.path.isdir(full_path):
+                #print "Entering folder: " + str(full_path)
+                self.addFolderToZip(zip_file, full_path)
 
 def main():
-	utilities = ZipUtilities()
-	filename = "processors1.zip"
-	directory = "processors1"
-	utilities.toZip(directory, filename)
-
+    utilities = ZipUtilities()
+    filename = "processors1.zip"
+    directory = ["processors1","Makefile"]
+    utilities.toZip(directory, filename)
+#cmdline = sys.argv[1:]
+#print cmdline
+#main(cmdline[0],cmdline[1])
 main()
--- extract_zip_folder.py
+++ extract_zip_folder.py
@@ -3,5 +3,6 @@ import zipfile
 def extract():
 	z = zipfile.ZipFile("processors1.zip","a")
 	z.extractall()
+	z.close()
 	
 extract()
